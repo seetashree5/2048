0d98e29522b117503412b1994526504eee215b77
lishuang <www.1030415864@qq.com>
2018-06-16 13:41:05 +0800

    fix bug

--- js/newScript.js
+++ js/newScript.js
@@ -1,9 +1,9 @@
-const mapDiv = document.getElementById('main')
+const mapDiv = document.getElementById('map')
 
-for(let i=1; i<4; i++){
-  let child = document.createElement('div')
-  child.id = 'box'
-  child.className = i
-  child.innerText = i
-  mapDiv.appendChild(child)
+for(let i=1; i<=4; i++){
+  for(let j=1; j<=4; j++){
+    let child = document.createElement('div')
+    child.className = 'box b'+i+j
+    mapDiv.appendChild(child)
+  }
 }
